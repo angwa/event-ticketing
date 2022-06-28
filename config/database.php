7fec09bbde136384fe739c52998f47432ebd774d
Angwa Moses <moses.angwa@9robee.com>
2022-06-28 21:10:09 +0100

    Setting configuration for heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,95 @@
 
 use Illuminate\Support\Str;
 
+$host = "us-cdbr-east-05.cleardb.net";
+$username = "bbb3cdb45b37ef:";
+$password = "cec29b08";
+$database = "heroku_e6e440de8b3adaa";
+
+//mysql://bbb3cdb45b37ef:cec29b08@us-cdbr-east-05.cleardb.net/heroku_e6e440de8b3adaa?reconnect=true
+
+if(getenv("APP_ENV") === "production"){
+  
+return [
+
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+            'mysql' => [
+                'driver' => 'mysql',
+                'host' => $host,
+                'port' => env('DB_PORT', '3306'),
+                'database' => $database,
+                'username' => $username,
+                'password' => $password,
+                'unix_socket' => env('DB_SOCKET', ''),
+                'charset' => 'utf8',
+                'collation' => 'utf8_general_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ],
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Migration Repository Table
+    |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run in the database.
+    |
+    */
+
+    'migrations' => 'migrations',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Redis Databases
+    |--------------------------------------------------------------------------
+    |
+    | Redis is an open source, fast, and advanced key-value store that also
+    | provides a richer body of commands than a typical key-value system
+    | such as APC or Memcached. Laravel makes it easy to dig right in.
+    |
+    */
+
+    'redis' => [
+
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+
+        'cache' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+    ],
+
+];
+
+ }
+
+
+
+
 return [
 
     /*
