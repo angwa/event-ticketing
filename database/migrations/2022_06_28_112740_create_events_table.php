e41ff4e3c24faae9df34a0504f5361f43d803c85
Angwa Moses <moses.angwa@9robee.com>
2022-06-28 15:41:21 +0100

    Migration and model

---
+++ database/migrations/2022_06_28_112740_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('location');
+            $table->float('price')->default(0.00);
+            $table->string('description')->nullable();
+            $table->integer('slots')->nullable();
+            $table->timestamp('date');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
